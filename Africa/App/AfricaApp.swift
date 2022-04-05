22303ab7498ca2aa3a0ff6077bd27c10a1af3f68
Anuj Soni <anujsoni10001@gmail.com>
2022-04-05 14:10:47 +0530

    Created Folder Structure & added required asset to project

---
+++ Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Anuj Soni on 05/04/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Africa/App/GalleryView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  Africa
+//
+//  Created by Anuj Soni on 05/04/22.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        GalleryView()
+    }
+}
